91f555b786dc6ebf52368d4a04fbfcbfb8f4f20d
10Montse10 <163039811+10montse10@users.noreply.github.com>
2024-12-18 17:06:49 -0600

    Programamos Cambio de Color

---
+++ script.js
@@ -0,0 +1,33 @@
+/* Obtener referencias en parrafo y al botón */
+const color = document.getElementById("color");
+const button = document.getElementById("button");
+
+/* manejar al evento */
+
+button.addEventListener("click", cambiarColor);
+
+
+
+function cambiarColor(){
+
+  /*  funcion añidada */
+  function colores (){
+    /* codigo quie se ejecuta */
+    const opciones = "0123456789ABCDEF"
+    let colorHex = "#";
+
+    /* Recorrer el string opciones  */
+    for (let i = 0; i < 6; i++){
+      let indicealeatorio = Math.floor(Math.random() * 16);
+      colorHex += opciones[indicealeatorio]; // #000000
+    }
+
+    return colorHex;
+  }
+
+  /* actualiza cambiar texto */
+  let coloraleatorio = colores();
+  /* actualiza cambiar color */
+  color.textContent = coloraleatorio;
+  document.body.style.backgroundColor = coloraleatorio;
+}
